9b95514600e9a2ac8d5c95bc6dcd8e33266ced32
Alexis Thiébaut <thiebautalexis01@gmail.com>
2021-10-02 18:29:33 +0200

    Cursor

---
+++ Assets/Core/Technical/PlayerCursor.cs
@@ -0,0 +1,20 @@
+// ===== Ludum Dare #49 - https://github.com/LucasJoestar/LudumDare49 ===== //
+//
+// Notes:
+//
+// ======================================================================== //
+
+using EnhancedEditor;
+using UnityEngine;
+
+namespace LudumDare49
+{
+	public class PlayerCursor : MonoBehaviour
+    {
+        #region Global Members
+        [Section("GameCursor")]
+
+        [SerializeField] private SpriteRenderer sprite = null;
+        #endregion
+    }
+}
