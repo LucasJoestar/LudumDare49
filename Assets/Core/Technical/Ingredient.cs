8f4e3a5e10a21850371d0a940d07e41341b4ae1c
LucasJoestar <lucasjoestar@outlook.fr>
2021-10-03 00:52:57 +0200

    Cursor & Tests

---
+++ Assets/Core/Technical/Ingredient.cs
@@ -0,0 +1,24 @@
+// ===== Ludum Dare #49 - https://github.com/LucasJoestar/LudumDare49 ===== //
+//
+// Notes:
+//
+// ======================================================================== //
+
+using EnhancedEditor;
+using UnityEngine;
+
+namespace LudumDare49
+{
+	public class Ingredient : PhysicsObject
+    {
+        #region Global Members
+        [Section("Ingredient")]
+
+        [SerializeField] private ActionPotion potion = null;
+        #endregion
+
+        #region Behaviour
+
+        #endregion
+    }
+}
