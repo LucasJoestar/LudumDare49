b17d9980c78d172cc662dd7e624590eb06bcb612
LucasJoestar <lucasjoestar@outlook.fr>
2021-10-04 23:06:11 +0200

    Pickup Lines + Book + Sounds

--- Assets/Core/Technical/PickupLine/PickupLine.cs
+++ Assets/Core/Technical/PickupLine/PickupLine.cs
@@ -9,35 +9,5 @@ using UnityEngine;
 
 namespace LudumDare49
 {
-    public class PickupLine : MonoBehaviour, IGrabbable
-    {
-        #region Global Members
-        [Section("PickupLine")]
-        [SerializeField, ReadOnly] private Transform handTransform = null;
-        private bool isGrabbed = false;
-        #endregion
-        public void Drop()
-        {
-            isGrabbed = false;
-        }
-
-        public void Grab(PlayerCursor _cursor, HingeJoint2D _joint)
-        {
-            isGrabbed = true;
-            handTransform = _joint.transform;
-            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        }
-
-        public void Shake()
-        {
-        }
-
-        private void Update()
-        {
-            if (isGrabbed)
-            {
-                transform.position = handTransform.position;
-            }
-        }
-    }
+    public class PickupLine : PhysicsObject { }
 }
